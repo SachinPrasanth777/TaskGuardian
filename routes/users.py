ed699505569193208afe3a274177df25b1341a6d
SachinPrasanth777 <142246653+SachinPrasanth777@users.noreply.github.com>
2024-07-05 11:12:13 +0530

    feat: login route

--- routes/users.py
+++ routes/users.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from utilities.database import Database
-from schema.users import CreateUserSchema
+from schema.users import CreateUserSchema, LoginSchema
 from utilities.response import JSONResponse
-from utilities.bcrypt import hash_password
+from utilities.bcrypt import hash_password, check_password
 
 db = Database()
 router = APIRouter()
@@ -12,7 +12,17 @@ router = APIRouter()
 async def handleSignUp(user: CreateUserSchema):
     email = db.users.find_one({"email": user.email})
     if email:
-        return JSONResponse({"success": False, "message": "User already Exists"})
+        raise HTTPException(status_code=400,detail="User Already Exists")
     hashed_password = hash_password(user.password).decode()
     db.users.insert_one({"email": user.email, "password": hashed_password})
     return JSONResponse({"success": True, "message": "User Signed Up Successfully"})
+
+@router.post("/login")
+async def handleLogin(user: LoginSchema):
+    email = db.users.find_one({"email": user.email})
+    if not email:
+        raise HTTPException(status_code=404,detail="User does not Exist")
+    value=check_password(user.password,email["password"])
+    if not value:
+        raise HTTPException(status_code=401,detail="Unauthorized Access/ Mismatched Passwords")
+    return JSONResponse({"success":True,"message":"User Logged in Successfully"})
--- schema/users.py
+++ schema/users.py
@@ -3,3 +3,7 @@ from pydantic import BaseModel,EmailStr
 class CreateUserSchema(BaseModel):
     email: EmailStr
     password: str
+
+class LoginSchema(BaseModel):
+    email: EmailStr
+    password: str
